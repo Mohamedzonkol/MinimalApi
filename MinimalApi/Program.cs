e79ab65a6911e540ee217fb187ad7d0944f73260
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-06-22 22:03:34 +0300

    post endpoint

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -8,8 +8,13 @@ builder.Services.AddDbContext<ToDoDbContext>(
 var app = builder.Build();
 app.MapGet("/todo-items", async (ToDoDbContext db) =>
     await db.ToDoItem.ToListAsync());
-app.MapGet("/todo-items/{id}", async (ToDoDbContext db, int id) =>
+app.MapGet("/todo-items/{id}", async (int id, ToDoDbContext db) =>
     await db.ToDoItem.FindAsync(id));
-
+app.MapPost("todo-items", async (ToDoItem item, ToDoDbContext db) =>
+{
+    await db.ToDoItem.AddAsync(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todo-items/{item.Id}", item);
+});
 
 app.Run();
